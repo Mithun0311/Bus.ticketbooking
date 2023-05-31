ea341013de9e847c3534d08b5f671ee8486c65c6
mithun796 <106151447+mithun796@users.noreply.github.com>
2023-05-31 22:44:37 +0600

    Add files via upload

---
+++ Bus Ticket Booking (213-35-796)/Login_info.java
@@ -0,0 +1,20 @@
+public class Login_info {
+    private String user_name = "mithun";
+    private String pass = "1234";
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
